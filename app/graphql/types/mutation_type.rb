f7188bc1c692aea2d56bb17cccfd42d0e3333a61
mikong <4162+mikong@users.noreply.github.com>
2020-01-13 21:15:50 +0800

    feat: Define SignInUser mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :sign_in_user, mutation: Mutations::SignInUser
     field :create_user, mutation: Mutations::CreateUser
     field :create_link, mutation: Mutations::CreateLink
   end
---
+++ test/graphql/mutations/sign_in_user_test.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Mutations::SignInUserTest < ActiveSupport::TestCase
+  def perform(args = {})
+    Mutations::SignInUser
+      .new(object: nil, field: nil, context: { session: {} })
+      .resolve(args)
+  end
+
+  def create_user
+    User.create!(
+      name: 'Test User',
+      email: 'email@example.com',
+      password: '[omitted]',
+    )
+  end
+
+  test 'success' do
+    user = create_user
+
+    result = perform(
+      email: {
+        email: user.email,
+        password: user.password
+      },
+    )
+
+    assert result[:token].present?
+    assert_equal result[:user], user
+  end
+
+  test 'failure because no credentials' do
+    assert_nil perform
+  end
+
+  test 'failure because wrong email' do
+    create_user
+    assert_nil perform(email: { email: 'wrong' })
+  end
+
+  test 'failure because wrong password' do
+    user = create_user
+    assert_nil perform(email: { email: user.email, password: 'wrong' })
+  end
+end
