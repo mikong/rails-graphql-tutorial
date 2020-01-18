b46811dd31fe3dfa2137e23563ca2d183d7c9f33
mikong <4162+mikong@users.noreply.github.com>
2020-01-18 11:21:23 +0800

    refactor: Enforce line length, other layout rules
    Layout/LineLength (80)
    Layout/EmptyLines
    Layout/ExtraSpacing
    Layout/SpaceAroundOperators
    Layout/SpaceInsideArrayLiteralBrackets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those
+  # in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -40,7 +41,6 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
